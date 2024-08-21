63874b3839d3ac336d831015578ab9a42a0cf773
Igor Leite <igorlfilgueiras@gmail.com>
2024-08-20 22:50:36 -0300

    exercicio de fixacao

---
+++ ListsFix/Program.cs
@@ -0,0 +1,54 @@
+﻿using ListsFix;
+using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Globalization;
+using System.Collections;
+using System.Diagnostics.Metrics;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        
+
+        List<employees> info = new List<employees>();
+
+        Console.Write("How many employees will be registered? ");
+        int quant = int.Parse(Console.ReadLine());
+
+        for (int i = 1; i <= quant; i++)
+        {
+            Console.WriteLine("Emplyoee #" + i + ":");
+            Console.Write("ID: ");
+            int id = int.Parse(Console.ReadLine());
+            Console.Write("Name: ");
+            string name = Console.ReadLine();
+            Console.Write("Salary: ");
+            double salary = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            info.Add(new employees(id, name, salary));
+            Console.WriteLine();
+            // Falta usar o find pra puxar um funcionario e alterar o seu salario
+            // se ele não existir, mostrar mensagem            
+        }
+        Console.Write("Enter the employee id that will have salary increase : ");
+        int idinfo = int.Parse(Console.ReadLine());
+        employees IDFind = info.Find(x => x.ID == idinfo);
+
+        if (IDFind != null)
+        {
+            Console.Write("Enter the percentage: ");
+            double porcentagem = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            IDFind.SalaryAdd(porcentagem);
+        }
+        else
+        {
+            Console.WriteLine("This id does not exist!");
+        }
+        Console.WriteLine("Updated list of employees:");
+        foreach (employees emp in info)
+        {
+            Console.WriteLine(emp);
+        }
+    }
+}
---
+++ ListsFix/employees.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace ListsFix
+{
+    internal class employees
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public double Salary { get; private set; }
+
+        public employees(int id, string name, double salary)
+        {
+            ID = id;
+            Name = name;
+            Salary = salary;
+        }
+        public void SalaryAdd(double percentage)
+        {
+            Salary += Salary * percentage / 100.0;
+        }
+
+        public override string ToString()
+        {
+            return "ID: "
+                + ID
+                + ", Name: "
+                + Name
+                + ", Salary: "
+                + Salary.ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
